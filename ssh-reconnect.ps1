bce55b11beeb1c0c2ac7d2f3a86048470541ebb9
general <generals.space@gmail.com>
2019-11-17 20:29:08 +0800

    修正: ssh隧道进程的创建从start-job换成start-process

--- ssh-reconnect.ps1
+++ ssh-reconnect.ps1
@@ -18,10 +18,18 @@ function get_ssh_proc {
 
 $exist = (get_ssh_proc forward-ssh | Measure-Object).Count
 if ($exist -eq 0) {
-    star-job -scriptblock {ssh forward-ssh}
+    ## 本来想尝试使用 start-job 启动ssh的后台进程的, 
+    ## 但是创建的job对象总是很快就变成`Completed`状态, 无法持续存在.
+    ## 也尝试过 {& ssh forward-ssh >console.out 2>console.err},
+    ## 仍然没用, 于是换用 start-process, 用效.
+    ## 参考文章 [Start a detached background process in PowerShell](https://stackoverflow.com/questions/25023458/start-a-detached-background-process-in-powershell)
+    ##
+    ## start-job -scriptblock {ssh forward-ssh}
+    Start-Process ssh -ArgumentList 'forward-ssh' -WindowStyle Hidden
 }
 
 $exist = (get_ssh_proc forward-rdp | Measure-Object).Count
 if ($exist -eq 0) {
-    star-job -scriptblock {ssh forward-rdp}
+    ## start-job -scriptblock {ssh forward-rdp}
+    Start-Process ssh -ArgumentList 'forward-rdp' -WindowStyle Hidden
 }
