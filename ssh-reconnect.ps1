f4c70806e5194496c5ef29f0f5308118bc0f3572
general <generals.space@gmail.com>
2020-06-22 14:34:43 +0800

    更新: 抽象转发进程验证方法

--- ssh-reconnect.ps1
+++ ssh-reconnect.ps1
@@ -6,14 +6,14 @@ $cronTrigger = New-JobTrigger -Once -RepeatIndefinitely -At (Get-Date) -Repetiti
 Add-JobTrigger -Name cronJob -Trigger $cronTrigger
 #>
 
-<#
-获取本机 ssh 进程状态
-#>
+
+## @function:   获取本机 ssh 转发进程对象
 function get_ssh_proc {
     param (
-        $ssh_target
+        ## 转发进程名, 一般为 forward-XXX
+        $ssh_proc
     )
-    return Get-WmiObject win32_process -filter "name = 'ssh.exe' and commandline like '%$ssh_target%'"
+    return Get-WmiObject win32_process -filter "name = 'ssh.exe' and commandline like '%$ssh_proc%'"
 }
 <#
 get_ssh_proc 上面是通过验证本地上的 ssh 进程存在以判断映射端口存在且正常的. 
@@ -29,47 +29,32 @@ function get_ssh_port {
     return ($port_list | Measure-Object).Count
 }
 
-$exist = (get_ssh_proc forward-ssh | Measure-Object).Count
-if ($exist -eq 0) {
-    ## 本来想尝试使用 start-job 启动ssh的后台进程的, 
-    ## 但是创建的job对象总是很快就变成`Completed`状态, 无法持续存在.
-    ## 也尝试过 {& ssh forward-ssh >console.out 2>console.err},
-    ## 仍然没用, 于是换用 start-process, 有效.
-    ## 参考文章 [Start a detached background process in PowerShell](https://stackoverflow.com/questions/25023458/start-a-detached-background-process-in-powershell)
-    ##
-    ## start-job -scriptblock {ssh forward-ssh}
-    Start-Process ssh -ArgumentList 'forward-ssh' -WindowStyle Hidden
-} else {
-    ## 如果进程存在, 则需要进一步验证端口映射是否成功, 否则杀掉该 ssh 进程
-    if (get_ssh_port 10002 -eq 0) {
-        $pid = (get_ssh_proc forward-ssh).ProcessId
-        Stop-Process $pid
+## @function:   保持转发进程的存在
+function keep_alive {
+    param (
+        ## 转发进程名, 一般为 forward-XXX
+        $ssh_proc,
+        ## 映射端口号
+        $ssh_port
+    )
+    $exist = (get_ssh_proc $ssh_proc | Measure-Object).Count
+    if ($exist -eq 0) {
+        ## 本来想尝试使用 start-job 启动ssh的后台进程的, 
+        ## 但是创建的job对象总是很快就变成`Completed`状态, 无法持续存在.
+        ## 也尝试过 {& ssh $ssh_proc >console.out 2>console.err},
+        ## 仍然没用, 于是换用 start-process, 有效.
+        ## 参考文章 [Start a detached background process in PowerShell](https://stackoverflow.com/questions/25023458/start-a-detached-background-process-in-powershell)
+        ##
+        ## start-job -scriptblock {ssh $ssh_proc}
+        Start-Process ssh -ArgumentList "$ssh_proc" -WindowStyle Hidden
+    } else {
+        ## 如果进程存在, 则需要进一步验证端口映射是否成功, 否则杀掉该 ssh 进程
+        if (get_ssh_port $ssh_port -eq 0) {
+            $pid = (get_ssh_proc $ssh_proc).ProcessId
+            Stop-Process $pid
+        }
     }
 }
-<#
-本机是家庭版, 不支持启动远程连接.
 
-$exist = (get_ssh_proc forward-rdp | Measure-Object).Count
-if ($exist -eq 0) {
-    ## start-job -scriptblock {ssh forward-rdp}
-    Start-Process ssh -ArgumentList 'forward-rdp' -WindowStyle Hidden
-} else {
-    ## 如果进程存在, 则需要进一步验证端口映射是否成功, 否则杀掉该 ssh 进程
-    if (get_ssh_port 10003 -eq 0) {
-        $pid = (get_ssh_proc forward-rdp).ProcessId
-        Stop-Process $pid
-    }
-}
-#>
-
-$exist = (get_ssh_proc forward-vnc | Measure-Object).Count
-if ($exist -eq 0) {
-    ## start-job -scriptblock {ssh forward-vnc}
-    Start-Process ssh -ArgumentList 'forward-vnc' -WindowStyle Hidden
-} else {
-    ## 如果进程存在, 则需要进一步验证端口映射是否成功, 否则杀掉该 ssh 进程
-    if (get_ssh_port 10003 -eq 0) {
-        $pid = (get_ssh_proc forward-vnc).ProcessId
-        Stop-Process $pid
-    }
-}
+keep_alive forward-ssh
+keep_alive forward-vnc
